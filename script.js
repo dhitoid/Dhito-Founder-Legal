1229f478bf58703f1ae855c9411ca4c789358e21
DT DEV ID <153781142+dhitoid@users.noreply.github.com>
2026-01-14 23:33:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ const observer = new IntersectionObserver(entries => {
 
 reveal.forEach(el => observer.observe(el));
 
+document.querySelectorAll('.btn').forEach(button => {
+  button.addEventListener('click', function (e) {
+    const ripple = document.createElement('span');
+    ripple.classList.add('ripple');
+
+    const rect = this.getBoundingClientRect();
+    ripple.style.left = `${e.clientX - rect.left}px`;
+    ripple.style.top = `${e.clientY - rect.top}px`;
+
+    this.appendChild(ripple);
+
+    setTimeout(() => ripple.remove(), 600);
+  });
+});
